6c999fa680f6cd07f8efb39fe1aa6b6c466359e4
modmarven <modmarven@gmail.com>
2023-12-20 05:26:02 -0800

    Update 4

---
+++ Assets/Plane Shooter/Script/UI Script/UiController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseGame;
+    [SerializeField]
+    private GameObject pauseButton;
+
+    void Start()
+    {
+        pauseGame.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        pauseGame.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0.0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseGame.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
