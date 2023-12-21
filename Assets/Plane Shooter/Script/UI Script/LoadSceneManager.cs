9d1993561f70f5d15c83927ff9fc139bd3b5431f
modmarven <modmarven@gmail.com>
2023-12-20 21:30:44 -0800

    Update 5

---
+++ Assets/Plane Shooter/Script/UI Script/LoadSceneManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    
+    public GameObject loadingManager;
+    public Slider progressSlider;
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadingProgress(index));
+    }
+
+    public IEnumerator LoadingProgress(int index)
+    {
+        progressSlider.value = 0;
+        loadingManager.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = progressSlider.value;
+
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+
+            if (progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+}
